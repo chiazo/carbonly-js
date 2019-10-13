5b0c0655d4ce3323631a09b4a6398b397fa3fa55
adsgh <unaxiomatical@gmail.com>
2019-10-13 00:42:29 -0400

    modified:   carbonly/screens/DescriptionScreen.js

--- carbonly/screens/DescriptionScreen.js
+++ carbonly/screens/DescriptionScreen.js
@@ -14,12 +14,12 @@ export default class DescriptionScreen extends React.Component {
 
     render() {
       return (
-        <View style={styles.container}>
+        <View style={styles.container, styles.textHierContainer}>
             <Button title="Offsets Options" onPress={() => this.props.navigation.navigate('Offsets')}/>
-            <Text style={styles.optionsTitleText}>Save Forests in Western Oaxaca, Mexico</Text>
-                <Text style={styles.optionCenteredText}>Mexico’s 65 million hectares of forest are disappearing at the rate of 1 million hectares a year. This project aims to mitigate environmental impacts caused by forest exploitation; restore areas with severe erosion or those affected by disease, fire, and pests; and to protect and prevent damage by fires, grazing, and illegal use of forest resources. The project is managed by a community of landowners who have pooled their resources.</Text>
-                <Text style={styles.optionCenteredText}></Text>
-                <Text style={styles.optionCenteredText}>Supporting this project costs 714 points per tonne of CO2e.</Text>
+            <Text style={styles.headerText}>Save Forests in Western Oaxaca, Mexico</Text>
+                <Text>Mexico’s 65 million hectares of forest are disappearing at the rate of 1 million hectares a year. This project aims to mitigate environmental impacts caused by forest exploitation; restore areas with severe erosion or those affected by disease, fire, and pests; and to protect and prevent damage by fires, grazing, and illegal use of forest resources. The project is managed by a community of landowners who have pooled their resources.</Text>
+                <Text></Text>
+                <Text>Supporting this project costs 714 points per tonne of CO2e.</Text>
                 <Button title="support this Project!" onPress={() => this.props.navigation.navigate('Purchase')}/>
         </View>
     );
