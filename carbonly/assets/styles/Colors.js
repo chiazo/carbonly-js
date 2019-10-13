0332194a7b910c74c978b1a05298f2f399851829
Anthony M <ajamesmichaels@gmail.com>
2019-10-13 09:39:53 -0400

    Abstracts colors into style file

---
+++ carbonly/assets/styles/Colors.js
@@ -0,0 +1,8 @@
+export default colors = {
+    back: '#161515',   // Black
+    pri: '#a8a7a7',    // Gray
+    contrast: '#e9e9e9', // Off-White
+    accent: '#2975f4', // Blue
+    accentAlt: '#1e56b2',    // Darker Blue
+    secAccent: '#1bc181', // Pine Green
+  };
