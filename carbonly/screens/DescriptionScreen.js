17d602acdc3123207a8140bcfd30c6e90284c4a8
adsgh <unaxiomatical@gmail.com>
2019-10-13 00:52:01 -0400

    modified:   carbonly/screens/DescriptionScreen.js 	modified:   carbonly/screens/PurchaseConfirmationScreen.js

--- carbonly/screens/DescriptionScreen.js
+++ carbonly/screens/DescriptionScreen.js
@@ -11,7 +11,7 @@ export default class DescriptionScreen extends React.Component {
     render() {
       return (
         <View style={styles.centerContainer}>
-            <Button title="Offsets Options" onPress={() => this.props.navigation.navigate('Offsets')}/>
+            <Button title="Offsets Options" onPress={() => this.props.navigation.navigate('Offset')}/>
             <Text style={styles.headerText}>Save Forests in Western Oaxaca, Mexico</Text>
                 <Text style={styles.baseText}>Mexico’s 65 million hectares of forest are disappearing at the rate of 1 million hectares a year. This project aims to mitigate environmental impacts caused by forest exploitation; restore areas with severe erosion or those affected by disease, fire, and pests; and to protect and prevent damage by fires, grazing, and illegal use of forest resources. The project is managed by a community of landowners who have pooled their resources.</Text>
                 <Text style={styles.baseText}></Text>
