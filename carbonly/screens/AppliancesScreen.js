ac4c6c19dd9ed4a29c2b4d5f1724b1abe66bfdf0
Anthony M <ajamesmichaels@gmail.com>
2019-10-12 22:15:31 -0400

    Adds logo and merges

--- carbonly/screens/AppliancesScreen.js
+++ carbonly/screens/AppliancesScreen.js
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
-import { StyleSheet, Text, View, Button } from 'react-native';
-=======
 import {View, Button, Text} from 'react-native';
 import styles from '../assets/styles/DefaultStyle';
->>>>>>> e83c53ac7f75b831ba4930cd40b08d6b3c1bde62
 
 export default class AppliancesScreen extends React.Component {
   render() {
