0a61b5d1a0961463b8e52cd0f00f82c27092de11
adsgh <unaxiomatical@gmail.com>
2019-10-12 22:44:21 -0400

    modified:   carbonly/screens/DescriptionScreen.js 	modified:   carbonly/screens/PurchaseConfirmationScreen.js

--- carbonly/screens/DescriptionScreen.js
+++ carbonly/screens/DescriptionScreen.js
@@ -12,9 +12,10 @@ export default class DescriptionScreen extends React.Component {
         <View style={styles.container}>
             <Button title="Offsets Options" onPress={() => this.props.navigation.navigate('Offsets')}/>
             <Text style={styles.optionsTitleText}>Save Forests in Western Oaxaca, Mexico</Text>
-                <Text style={styles.optionText}>Mexico’s 65 million hectares of forest are disappearing at the rate of 1 million hectares a year. This project aims to mitigate environmental impacts caused by forest exploitation; restore areas with severe erosion or those affected by disease, fire, and pests; and to protect and prevent damage by fires, grazing, and illegal use of forest resources. The project is managed by a community of landowners who have pooled their resources.</Text>
-                <Text style={styles.optionText}>Supporting this project costs 714 points per tonne of CO2e.</Text>
-                <Button title="Purchase 70 kilograms of CO2e offset using 50 points" onPress={() => this.props.navigation.navigate('Purchase')}/>
+                <Text style={styles.optionCenteredText}>Mexico’s 65 million hectares of forest are disappearing at the rate of 1 million hectares a year. This project aims to mitigate environmental impacts caused by forest exploitation; restore areas with severe erosion or those affected by disease, fire, and pests; and to protect and prevent damage by fires, grazing, and illegal use of forest resources. The project is managed by a community of landowners who have pooled their resources.</Text>
+                <Text style={styles.optionCenteredText}></Text>
+                <Text style={styles.optionCenteredText}>Supporting this project costs 714 points per tonne of CO2e.</Text>
+                <Button title="support this Project!" onPress={() => this.props.navigation.navigate('Purchase')}/>
         </View>
     );
   }
--- carbonly/screens/PurchaseConfirmationScreen.js
+++ carbonly/screens/PurchaseConfirmationScreen.js
@@ -11,6 +11,13 @@ export default class PurchaseConfirmationScreen extends React.Component {
       return (
         <View style={styles.container}>
             <Button title="Offsets Options" onPress={() => this.props.navigation.navigate('Offsets')}/>
+            <Text style={styles.optionsTitleText}>Purchase Confirmation</Text>
+            <Text style={styles.optionCenteredText}>You may purchase this offset in 50-point increments.</Text>
+            <Text style={styles.optionCenteredText}>Each increment results in a 70-kilogram carbon offset being bought.</Text>
+            <Button title="Purchase 70 kilograms of carbon offset." onPress={() => {
+                this.props.navigation.navigate('');
+                alert("Congrats! You have purchased 70 kilograms of carbon offset, supporting the forests of Western Oaxaca, Mexico.");
+            }}></Button>
         </View>
     );
   }
