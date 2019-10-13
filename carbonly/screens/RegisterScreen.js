504483c251a0aa5aa17b96da0482f83a4f59022d
chiazo <chiazo@live.unc.edu>
2019-10-13 10:44:06 -0400

    passed user and pass to server

--- carbonly/screens/RegisterScreen.js
+++ carbonly/screens/RegisterScreen.js
@@ -30,13 +30,13 @@ export default class RegisterScreen extends React.Component {
 
   handleSubmit = () => {
     const { user, pass }  = this.state ;
-    Alert.alert(user+" "+pass);
+    // Alert.alert(user+" "+pass);
 
-   /* fetch('https://dioxidely-data-beta.herokuapp.com/register', {
+    fetch('https://dioxidely-data-beta.herokuapp.com/register', {
       method: 'POST',
       data: {
-        email: self.refs.email,
-        password: self.refs.password
+        email: user,
+        password: pass
       }
     })
       .then(function (response) {
@@ -44,7 +44,7 @@ export default class RegisterScreen extends React.Component {
       }).then(function (body) {
         console.log(body);
       });
-      */
+      
   };
 
   render() {
